d118879a08b319f0f1608b5b8d7c0de93c1b699b
Nicholas S Nociforo <nicholassnociforo@Nicholas-Nociforos-MacBook-Pro.local>
2017-07-28 20:53:26 -0700

    U

---
+++ routes/api-routes.js
@@ -0,0 +1,30 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Grabbing our models
+
+// var db = require("../models");
+
+// module.exports = function(app) {
+
+//   app.get("/api/todos", function(req, res) {
+
+//   });
+
+//   app.post("/api/todos", function(req, res) {
+
+//   });
+
+//   app.delete("/api/todos/:id", function(req, res) {
+
+//   });
+
+  
+//   app.put("/api/todos", function(req, res) {
+
+//   });
+// };
